d057189eb8c0e16a93cb3946847e5062ad485c72
David Rios <david@davidrios.co>
2025-05-12 17:16:48 -0500

    ✨ Feat: Added function to select the best image from srcSet of images

---
+++ src/utils/utils.ts
@@ -0,0 +1,26 @@
+export const getBestImageFromSrcSet = (flatSrcSet: string, minSize: number) => {
+
+    // Turn srcSet string into an array containing both link and size of each image
+  const srcSet = flatSrcSet.split(",");
+
+  // Iterate each (link + size) string, and push "size" value into an array
+  const srcSetSizes: Array<string> = [];
+  srcSet.map((set) => {
+    srcSetSizes.push(set.trim().split(" ")[1].slice(0, -1));
+  });
+
+  // Calculate how close is each size to IMAGE_MIN_WIDTH
+  const closeness : Array<number> = [];
+  srcSetSizes.map((size) => {
+    closeness.push(Number(size) - minSize);
+  });
+
+  // Eliminate negative closeness and order from loweest to highest valvue
+  const filteredCloseness = closeness.filter((close) => (close > 0));
+  filteredCloseness.sort((a, b) => (a - b));
+
+  // Get best image from srcSet
+  const bestIndex = closeness.indexOf(filteredCloseness[0]);
+
+  return srcSet[bestIndex].trim().split(' ')[0];
+}
