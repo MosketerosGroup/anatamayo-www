21145742d064a65f80dcbc1671be5882b8a87c45
David Rios <david@davidrios.co>
2025-05-12 20:44:10 -0500

    💬 Text: Fixed wrong word

--- src/components/widgets/Subscribe.tsx
+++ src/components/widgets/Subscribe.tsx
@@ -2,7 +2,7 @@ export default function Subscribe() {
   return (
     <div className="ml-10 mt-10 text-right">
       <div className="md:animate-wobble">
-        <a href="https://forms.gle/9tvbr58DKjWEpiHc7" target="_blank">Subscribe<br/>to newletter</a>
+        <a href="https://forms.gle/9tvbr58DKjWEpiHc7" target="_blank">Subscribe<br/>to newsletter</a>
       </div>
       <hr className="my-2"/>
       <a href="#!">ES</a> / <a href="#!">EN</a>
