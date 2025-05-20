6680b4a17038a1d57edef0a64d8022175e9657d0
David Rios <david@davidrios.co>
2025-05-20 14:52:41 -0500

    💄 UI: ContentLoader component is shown centered

--- src/lib/ContentLoader.tsx
+++ src/lib/ContentLoader.tsx
@@ -5,21 +5,23 @@ const ContentLoader = ({width = "100%", height = "100", viewBoxX = "100", viewBo
   const viewBox = `0 0 ${viewBoxX} ${viewBoxY}`;
 
   return (
-  <Loader
-    title="Loading..."
-    speed={4}
-    width={width}
-    height={height}
-    viewBox={viewBox}
-    backgroundColor="#00000000"
-    foregroundColor="#DEDEDE"
-    backgroundOpacity={0}
-    foregroundOpacity={0.25}
-    gradientRatio={2}
-    {...props}
-  >
-    <rect transform={transform} x="13" y="4" rx="0" ry="0" width={width} height={height} />
-  </Loader>
+  <div className="mx-auto">
+    <Loader
+      title="Loading..."
+      speed={4}
+      width={width}
+      height={height}
+      viewBox={viewBox}
+      backgroundColor="#00000000"
+      foregroundColor="#DEDEDE"
+      backgroundOpacity={0}
+      foregroundOpacity={0.25}
+      gradientRatio={2}
+      {...props}
+    >
+      <rect transform={transform} x="13" y="4" rx="0" ry="0" width={width} height={height} />
+    </Loader>
+  </div>
 )};
 
 export default ContentLoader
