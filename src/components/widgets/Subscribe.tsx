9bb31c969ea3cb071972e8a56a3440141c9a7eb5
David Rios <david@davidrios.co>
2025-05-21 18:48:41 -0500

    💄 UI: Hide "EN/ES" link for a while

--- src/components/widgets/Subscribe.tsx
+++ src/components/widgets/Subscribe.tsx
@@ -4,8 +4,8 @@ export default function Subscribe() {
       <div className="md:animate-wobble">
         <a href="https://forms.gle/9tvbr58DKjWEpiHc7" target="_blank">Subscribe<br/>to newsletter</a>
       </div>
-      <hr className="my-2"/>
-      <a href="#!">ES</a> / <a href="#!">EN</a>
+      {/* <hr className="my-2"/>
+      <a href="#!">ES</a> / <a href="#!">EN</a> */}
     </div>
   );
 }
